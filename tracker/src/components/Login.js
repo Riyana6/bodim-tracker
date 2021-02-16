ad76ba76ea65d29bc17bef750fac71669309e2f8
JMJ Riyana <jeemariyana@gmail.com>
2021-02-16 14:15:50 +0530

    updated

--- tracker/src/components/Login.js
+++ tracker/src/components/Login.js
@@ -1,37 +1,66 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import {Row, Col, Card, Form, InputGroup, FormControl, Button, Alert} from 'react-bootstrap';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faSignInAlt, faEnvelope, faLock, faUndo} from "@fortawesome/free-solid-svg-icons";
 
 export default class Login extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state =this.initialState;
+    }
+
+    initialState = {
+        email: '', password:''
+    };
+
     render() {
+        const {email, password }= this.state;
         return (
-            <div className="auth-wrapper">
-                <div className="auth-inner">
-                    <form>
-                        <h3>Log In</h3>
-
-                        <div className="form-group">
-                            <label>Email address</label>
-                            <input type="email" className="form-control" placeholder="Enter email" />
-                        </div>
-
-                        <div className="form-group">
-                            <label>Password</label>
-                            <input type="password" className="form-control" placeholder="Enter password" />
-                        </div>
-
-                        <div
-                        className="form-group">
-                            <div className="custom-control custom-checkbox">
-                                <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                                <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
-                            </div>
-                        </div>
-
-                        <button type="submit" className="btn btn-primary btn-block"><a href="/UserHome">Submit</a></button>
-                        
-                    </form>
-                </div>
-            </div>
-            
+            <Row className="justify-content-md-center">
+                <Col xs={5}>
+                    <Card className={"border border-dark bg-dark text-white"}>
+                        <Card.Header>
+                            <FontAwesomeIcon icon={faSignInAlt}/> Login
+                        </Card.Header>
+                        <Card.Body>
+                            <Form.Row>
+                                <Form.Group as={Col}>
+                                    <InputGroup>
+                                        <InputGroup.Prepend>
+                                            <InputGroup.Text><FontAwesomeIcon icon={faEnvelope}/></InputGroup.Text>
+                                        </InputGroup.Prepend>
+                                        <FormControl required autoComplete="off" type="text" name="email" value={email}
+                                            className={"bg-dark text-white"} placeholder="Enter Email Address"/>
+                                    </InputGroup>
+                                </Form.Group>
+                            </Form.Row>
+                            <Form.Row>
+                                <Form.Group as={Col}>
+                                    <InputGroup>
+                                        <InputGroup.Prepend>
+                                            <InputGroup.Text><FontAwesomeIcon icon={faLock}/></InputGroup.Text>
+                                        </InputGroup.Prepend>
+                                        <FormControl required autoComplete="off" type="password" name="password" value={password}
+                                            className={"bg-dark text-white"} placeholder="Enter Password"/>
+                                    </InputGroup>
+                                </Form.Group>
+                            </Form.Row>
+                        </Card.Body>
+                        <Card.Footer style={{"text-align":"right"}}>
+                            <Button size="sm" type="button" variant="success">
+                                <FontAwesomeIcon icon={faSignInAlt}/> Login
+                            </Button>{' '}
+                            <Button size="sm" type="button" variant="info">
+                                <FontAwesomeIcon icon={faUndo}/> Reset
+                            </Button>
+                        </Card.Footer>
+                    </Card>
+                </Col>
+            </Row>
         );
     }
 }
+
+
